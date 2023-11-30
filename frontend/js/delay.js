10329c67595258f00ded4f1943728e5fb24097f9
Violetta Gejno <vgejno@student.42lausanne.ch>
2023-11-30 23:39:12 +0100

    bootstrap home

---
+++ frontend/js/delay.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    setTimeout(function() {
+        var buttonContainer = document.getElementById('delayed-button-container');
+        buttonContainer.classList.remove('hidden');
+    }, 3000);
+});
+
